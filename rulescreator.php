fc4f9cc6d82970c5ec199c9e9687897d0221584c
Eduardo Fernández <eduardofernandezsoy@gmail.com>
2016-02-13 23:16:02 +0100

    The beginning
    Creation of a few files to do all the program.
    - "index.html" will be the visible page.
    - "prerules.php" the code for catch and order the previous iptables
    rules.
    - "rulescreator.php" will create the rules from the file uploaded and
    shuffle with the previous rules if exists.
    - "search.php" will try to find the ips to ban from the file uploaded.

---
+++ rulescreator.php
@@ -0,0 +1,8 @@
+<?php
+
+/* 
+ * Made by: Eduardo Fernandez
+ * If you like what I do thank me on twitter!
+ * @EduFdezSoy
+ */
+
---
+++ search.php
@@ -0,0 +1,7 @@
+<?php
+
+/* 
+ * Made by: Eduardo Fernandez
+ * If you like what I do thank me on twitter!
+ * @EduFdezSoy
+ */
