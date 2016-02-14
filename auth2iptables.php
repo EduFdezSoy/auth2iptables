6abfa6ac244322c6786c31c9b6d867608b956d36
Eduardo Fernández <eduardofernandezsoy@gmail.com>
2016-02-14 21:56:37 +0100

    includes
    added includes

--- auth2iptables.php
+++ auth2iptables.php
@@ -5,3 +5,14 @@
  * If you like what I do thank me on twitter!
  * @EduFdezSoy
  */
+
+$log = $_POST["log"];       // auth.log file
+$tab = $_POST["iptables"];  // iptables input
+
+include ("search.php");
+
+include ("prerules.php");
+
+include ("rulescreator.php");
+
+?>
